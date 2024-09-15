02f32c3709184dd4d8b12b3dcfbceb2939781961
Kavoshnik <hikkan008@gmail.com>
2024-09-15 15:39:15 +0700

    new file:   .vscode/c_cpp_properties.json 	new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	modified:   binary search.cpp

--- binary search.cpp
+++ binary search.cpp
@@ -1,38 +1,44 @@
 #include <iostream>
 #include <algorithm>
 
-using namespace std;
-
-int main() {
+int main()
+{
 
     int arr[10]; // создали массив на 10 элементов
-    int key; // создали переменную в которой будет находиться ключ
+    int key;     // создали переменную в которой будет находиться ключ
 
-    cout << "enter 10 number for massive: " << endl;
+    std::cout << "enter 10 number for massive: " << std::endl;
 
-    for (int i = 0; i < 10; i++) {
-        cin >> arr[i]; // считываем элементы массива
+    for (int i = 0; i < 10; i++)
+    {
+        std::cin >> arr[i]; // считываем элементы массива
     }
 
-    sort(arr, arr + 10); // сортируем с помощью функции sort (быстрая сортировка)
+    std::sort(arr, arr + 10); // сортируем с помощью функции sort (быстрая сортировка)
 
-    cout << endl << "enter key: ";
+    std::cout << "enter key: " << std::endl;
 
-    cin >> key; // считываем ключ
+    std::cin >> key; // считываем ключ
 
     bool flag = false;
     int l = 0; // левая граница
     int r = 9; // правая граница
     int mid;
 
-    while ((l <= r) && (flag != true)) {
+    while ((l <= r) && (flag != true))
+    {
         mid = (l + r) / 2; // считываем срединный индекс отрезка [l,r]
 
-        if (arr[mid] == key) flag = true; //проверяем ключ со серединным элементом
-        if (arr[mid] > key) r = mid - 1; // проверяем, какую часть нужно отбросить
-        else l = mid + 1;
+        if (arr[mid] == key)
+            flag = true; // проверяем ключ со серединным элементом
+        if (arr[mid] > key)
+            r = mid - 1; // проверяем, какую часть нужно отбросить
+        else
+            l = mid + 1;
     }
 
-    if (flag) cout << "element index " << key << " in massive = " << mid << endl;
-    else cout << "Not Found" << endl;
+    if (flag)
+        std::cout << "element index " << key << " in massive = " << mid << std::endl;
+    else
+        std::cout << "Not Found" << std::endl;
 }
