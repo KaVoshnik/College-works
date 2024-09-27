3d01f33629d2b3f9e1e81f3b3256610f9b1b554c
Kirill <112951242+KaVoshnik@users.noreply.github.com>
2024-09-27 14:59:55 +0000

    Update control_work 10.10.23 second.cpp

--- control_work 10.10.23 second.cpp
+++ control_work 10.10.23 second.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) {
             return false;
         }
+
         int reverse = 0;
-        int current = x;
-        while (current > 0) {
-            int a = current % 10;
-            current /= 10;
-            if (reverse < INT_MAX / 10 || (reverse == INT_MAX % 10 && a <= INT_MAX % 10)) {
-                reverse - reverse * 10 + a;
-            }
-            else {
-                return false;
-            }
+        
+        while (x > reverse) {
+            reverse = reverse * 10 + x % 10;
+            x /= 10;
         }
-        return reverse == x;
+        return (x == reverse) || (x == reverse / 10);
     }
 };
